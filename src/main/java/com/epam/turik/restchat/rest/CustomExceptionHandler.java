fd945e0430f7df067069f8f6d858b4d575764218
ilya turov <meetyourturik@gmail.com>
2021-07-06 22:48:06 +0300

    proper response error code in headers

--- src/main/java/com/epam/turik/restchat/rest/CustomExceptionHandler.java
+++ src/main/java/com/epam/turik/restchat/rest/CustomExceptionHandler.java
@@ -5,11 +5,9 @@ import com.epam.turik.restchat.rest.objects.ExceptionDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindException;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 @Slf4j
 @RestControllerAdvice
@@ -17,20 +15,14 @@ public class CustomExceptionHandler {
 
     // wrong enum
     @ExceptionHandler(value = BindException.class)
-    public ExceptionDTO handleEnum(BindException ex, HttpServletRequest request) {
-        // somehow determine enum class from exception and provide viable options
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public ExceptionDTO handleEnum(BindException ex) {
         return new ExceptionDTO("BindException","Wrong ENUM parameter in request", HttpStatus.BAD_REQUEST.value());
     }
 
     @ExceptionHandler(value = UserNotFoundException.class)
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
     public ExceptionDTO handleUserNotFound(UserNotFoundException e) {
         return new ExceptionDTO("UserNotFoundException","can't find user with id " + e.getId(), HttpStatus.NOT_FOUND.value());
     }
-
-    /*
-    @ExceptionHandler(value = Exception.class)
-    public ExceptionDTO handleDefault(HttpServletRequest request, Exception e) {
-        return new ExceptionDTO("Default Exception","something went wrong, dunno what tho", HttpStatus.INTERNAL_SERVER_ERROR.value());
-    }
-    */
 }
