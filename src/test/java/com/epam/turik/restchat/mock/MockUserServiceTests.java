6f7e0c28bae9087ade5d01690db32338b788cb28
turik <meetyourturik@gmail.con>
2021-11-11 22:41:13 +0300

    mock tests

--- src/test/java/com/epam/turik/restchat/mock/MockUserServiceTests.java
+++ src/test/java/com/epam/turik/restchat/mock/MockUserServiceTests.java
@@ -6,6 +6,10 @@ import com.epam.turik.restchat.model.UpdateService;
 import com.epam.turik.restchat.model.UserModelMapper;
 import com.epam.turik.restchat.model.UserService;
 import com.epam.turik.restchat.model.objects.user.User;
+import com.epam.turik.restchat.model.objects.user.UserUpdate;
+import com.epam.turik.restchat.rest.objects.UserFilter;
+import com.epam.turik.restchat.types.user.ChatPermission;
+import com.epam.turik.restchat.types.user.UserStatus;
 import lombok.extern.slf4j.Slf4j;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -19,7 +23,10 @@ import org.junit.jupiter.api.parallel.ExecutionMode;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Example;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.mockito.Mockito.*;
@@ -50,16 +57,12 @@ class MockUserServiceTests {
         user.setUsername("turik");
         user.setId(id);
         // when
-        /*
-        UserEntity userEntity = userModelMapper.toEntity(user);
-        UserEntity returnUser = userRepository.save(userEntity);
-        return userModelMapper.fromEntity(returnUser);
-         */
         when(userModelMapper.toEntity(user)).thenReturn(userEntity);
         when(userRepository.save(userEntity)).thenReturn(userEntity);
         when(userModelMapper.fromEntity(userEntity)).thenReturn(user);
         // then
         User result = userService.createUser(user);
+
         assertEquals(result, user);
         verify(userModelMapper, times(1)).toEntity(user);
         verify(userModelMapper, times(1)).fromEntity(userEntity);
@@ -85,125 +88,85 @@ class MockUserServiceTests {
        i.e. the eq() is not necessary and can be omitted
      */
         when(userRepository.findUserById(id)).thenReturn(Optional.of(userEntity));
-        when(userModelMapper.fromEntity(userEntity)).thenReturn(user);
+        when(userModelMapper.fromEntity(any(UserEntity.class))).thenReturn(user);
         // then
         User result = userService.getUserById(id);
 
         assertEquals(result, user);
         verify(userRepository, times(1)).findUserById(id);
-        verify(userModelMapper, times(1)).fromEntity(userEntity);
+        verify(userModelMapper, times(1)).fromEntity(any(UserEntity.class));
     }
 
+    @Test
+    @DisplayName("should return all users that were added")
+    void getSomeUsers() {
+        // given
+        List<User> users = new ArrayList<>();
+        User user = new User();
+        user.setUsername("turik");
+        users.add(user);
+        //when
+        when(userModelMapper.fromEntityList(anyList())).thenReturn(users);
+        // then
+        List<User> result = userService.getAllUsers();
+        assertEquals(result, users);
 
-    @Nested
-    @DisplayName("getAllUsers tests")
-    class GetAllUsersUsersTest {
-        @Test
-        @DisplayName("should return empty list when no users")
-        void getZeroUsersTest() {
-
-        }
-
-        @Test
-        @DisplayName("should return all users that were added")
-        void getSomeUsers() {
-
-        }
+        verify(userRepository, times(1)).findAll();
+        verify(userModelMapper, times(1)).fromEntityList(anyList());
     }
 
-    @Nested
-    @DisplayName("getUsersByFilter tests")
-    class GetUsersByFilterTest {
-        @Test
-        @DisplayName("should find users by username pattern")
-        void getUsersByUsername() {
+    @Test
+    @DisplayName("should find users by filter with all parameters")
+    void getUsersByAllParams() {
+        // given
+        UserFilter filter = new UserFilter();
+        filter.setUsername("mo");
+        filter.setLanguage("en");
+        filter.setStatus(UserStatus.ACTIVE);
+        filter.setChatPermission(ChatPermission.FRIENDS_ONLY);
 
-        }
+        List<User> users = new ArrayList<>();
+        User user = new User();
+        user.setUsername("turik");
+        users.add(user);
+        //when
+        when(userModelMapper.fromEntityList(anyList())).thenReturn(users);
 
-        @Test
-        @DisplayName("should find users by language pattern")
-        void getUsersByLanguage() {
+        // then
+        List<User> result = userService.getUsersByFilter(filter);
+        assertEquals(result, users);
 
-        }
-
-        @Test
-        @DisplayName("should find users by status pattern")
-        void getUsersByStatus() {
-
-        }
-
-        @Test
-        @DisplayName("should find users by chatPermission pattern")
-        void getUsersByChatPermission() {
-
-        }
-
-        @Test
-        @DisplayName("should find users by username and language pattern")
-        void getUsersByUsernameAndLanguage() {
-
-        }
-
-        @Test
-        @DisplayName("should find users by username, language and status pattern")
-        void getUsersByUsernameAndLanguageAndStatus() {
-
-        }
-
-        @Test
-        @DisplayName("should find users by filter with all parameters")
-        void getUsersByAllParams() {
-
-        }
-
-        @Test
-        @DisplayName("should return empty list when no users found")
-        void getZeroUsers() {
-
-        }
+        verify(userRepository, times(1)).findAll(any(Example.class));
+        verify(userModelMapper, times(1)).fromEntityList(anyList());
     }
 
-    @Nested
-    @DisplayName("updateUser tests")
-    class UpdateUserTest {
-        @Test
-        @DisplayName("should successfully update String fields of User")
-        void updateStringFields() {
-
-        }
-
-        @Test
-        @DisplayName("should successfully update Enum fields of User")
-        void updateEnumFields() {
-
-        }
-
-        @Test
-        @DisplayName("should successfully update Timestamp fields of User")
-        void updateTimestampFields() {
-
-        }
-
-        @Test
-        @DisplayName("should successfully remove field")
-        void removeField() {
-
-        }
+    @Test
+    @DisplayName("should successfully update fields of User")
+    void updateTimestampFields() {
+        // given
+        long id = 1L;
+        UserUpdate patch = new UserUpdate();
+        patch.setStatus(Optional.empty());
+        // when
+        when(userRepository.findUserById(anyLong())).thenReturn(Optional.of(new UserEntity()));
+        when(userModelMapper.fromEntity(any(UserEntity.class))).thenReturn(new User());
+        when(userModelMapper.toEntity(any(User.class))).thenReturn(new UserEntity());
+        // then
+        User result = userService.updateUser(id, patch);
+        verify(updateService, times(1)).applyUpdate(any(User.class), any(UserUpdate.class));
+        verify(userModelMapper, times(1)).toEntity(any(User.class));
+        verify(userRepository, times(1)).findUserById(anyLong());
+        verify(userRepository, times(1)).save(any(UserEntity.class));
+        verify(userModelMapper, times(1)).fromEntity(any(UserEntity.class));
     }
 
-    @Nested
-    @DisplayName("deleteUser tests")
-    class DeleteUserTest {
-        @Test
-        @DisplayName("should successfully delete existing user")
-        void deleteExistingUser() {
-
-        }
-
-        @Test
-        @DisplayName("should do nothing when deleting nonexistent user")
-        void deleteNonExistingUser() {
-
-        }
+    @Test
+    @DisplayName("should successfully delete existing user")
+    void deleteExistingUser() {
+        // given
+        // when
+        userService.deleteUser(anyLong());
+        // then
+        verify(userRepository, times(1)).deleteById(anyLong());
     }
 }
