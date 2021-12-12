51850b7ac118d4fd4089a50d513a4cb3bc9b75c0
ilya turov <meetyourturik@gmail.com>
2021-12-12 22:18:42 +0300

    kida working docker

---
+++ docker/wait-for-postgres.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -e
+>&2 echo "gotta check pg"
+host="$1"
+shift
+
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$host" -p 5438 -U "postgres" -c '\q'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Postgres is up - executing command"
+exec "$@"
